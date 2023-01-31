99d9f095fd949a537c4a053f299e1a592b516b14
Yvonne Adedeji <yvonne.sarah.adedeji@hotmail.com>
2023-01-31 19:28:50 +0000

    Updated script.js - with working function for 5 days forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
-var apiKey = "23e942d86d90cbac1cd7f900775196d1";
-var today = moment().format('D/M/Y');
+var APIKey = "23e942d86d90cbac1cd7f900775196d1";
+var today = moment().format('DD/MM/YYYY');
 var searchedCityHistoryList = [];
 
 // function for current condition
 function currentCondition(city) {
 
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+var currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
 
 $.ajax({
-url: queryURL,
+url: currentURL,
 method: "GET"
 }).then(function(cityWeather) {
 console.log(cityWeather);
+var lat = cityWeather.coord.lat;
+var lon = cityWeather.coord.lon;
+fiveDaysCondition(lat,lon);
 $("#weather").css("display", "block");
 $("#cityDetails").empty();
         
@@ -35,42 +38,46 @@ $("#cityDetails").append(currentCity);
 // function for 5 days condition
 function fiveDaysCondition(lat, lon) {
 
-    var futureURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+var fiveDaysURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly,alerts}&appid=${APIKey}`;
 
-    $.ajax({
-        url: futureURL,
-        method: "GET"
-    }).then(function(fiveDaysCondition) {
-        console.log(fiveDaysCondition);
-        $("#CityfiveDays").empty();
+$.ajax({
+url: fiveDaysURL,
+method: "GET"
+}).then(function(fiveDaysCondition) {
+console.log(fiveDaysCondition);
+$("#CityfiveDays").empty();
         
-        for (let i = 1; i < 6; i++) {
-            var cityInfo = {
-                date: fiveDaysCondition.daily[i].dt,
-                icon: fiveDaysCondition.daily[i].weather[0].icon,
-                temp: fiveDaysCondition.daily[i].temp.day,
-                humidity: fiveDaysCondition.daily[i].humidity
-            };
+for (let i = 1; i < 6; i++) {
+var cityInformation = {
+date: fiveDaysCondition.daily[i].dt,
+icon: fiveDaysCondition.daily[i].weather[0].icon,
+temp: fiveDaysCondition.daily[i].temp.day,
+wind: fiveDaysCondition.daily[i].wind_speed,
+humidity: fiveDaysCondition.daily[i].humidity
+};
 
-            var currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
-            var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
-
-            var futureCard = $(`
-                <div class="pl-3">
-                    <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
-                        <div class="card-body">
-                            <h5>${currDate}</h5>
-                            <p>${iconURL}</p>
-                            <p>Temp: ${cityInfo.temp} °C</p>
-                            <p>Humidity: ${cityInfo.humidity}\%</p>
-                        </div>
-                    </div>
-                <div>
-            `);
-
-            $("#CityfiveDays").append(futureCard);
-        }
-    }); 
+var currentDate = moment.unix(cityInformation.date).format("DD/MM/YYYY");
+var iconURL = `<img src="https://openweathermap.org/img/w/${cityInformation.icon}.png" alt="${fiveDaysCondition.daily[i].weather[0].main}" />`;
+// displays the date
+// an icon representation of weather conditions
+// the temperature
+// the Wind speed
+// the humidity
+var fiveDaysCard = $(`
+<div class="card pl-3">
+<div class="card-body">
+<h3>${currentDate}</h3>
+<p>${iconURL}</p>
+<p>Temp: ${cityInformation.temp} °C</p>
+<p>Wind: ${cityInformation.wind} KPH</p>
+<p>Humidity: ${cityInformation.humidity}\%</p>
+</div>
+</div>
+<div>
+`);
+$("#CityfiveDays").append(fiveDaysCard);
+}
+}); 
 }
 
 // add on click event listener 
@@ -92,7 +99,7 @@ console.log(searchedCityHistoryList);
 });
 
 // WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// THEN I am again presented with current and fivedays conditions for that city
 $(document).on("click", ".list-group-item", function() {
 var CityList = $(this).text();
 currentCondition(CityList);
